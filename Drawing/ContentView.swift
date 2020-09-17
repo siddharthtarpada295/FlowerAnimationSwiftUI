48424148358f008e3387497d6d0b357342c18f46
Siddharth Tarpada <siddharthtarpada295@gmail.com>
2020-09-17 09:58:01 +0530

    remove Triangle file

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,32 +7,51 @@
 //
 
 import SwiftUI
-struct Triangle: Shape {
+struct FlowerShape: Shape {
+    var petalOffset: Double = -20
+    var petalWidth: Double = 100
+    
     func path(in rect: CGRect) -> Path {
         var path = Path()
         
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
-        
+        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8) {
+            let rotation = CGAffineTransform(rotationAngle: number)
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            
+            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width / 2))
+            let rotatedPetal = originalPetal.applying(position)
+            
+            path.addPath(rotatedPetal)
+            
+        }
         return path
     }
 }
 struct ContentView: View {
-    var body: some View {
-        NavigationView {
-                VStack {
-                        Triangle()
-                        .stroke(Color.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
-                        .frame(width: 300, height: 300)
-                }
-            .navigationBarTitle("Triangle", displayMode: .inline)
-                .navigationBarItems(trailing: NavigationLink(destination: Flower()) {
-                    Text("Next")
-            })
+    @State private var petalOffset: Double = -20
+        @State private var petalWidth: Double = 100.0
+        
+        var body: some View {
+            NavigationView {
+                    VStack {
+                        FlowerShape(petalOffset: petalOffset, petalWidth: petalWidth)
+                            .fill(Color.red, style: FillStyle(eoFill: true))
+    //                        .stroke(Color.black, lineWidth: 1)
+
+                        
+                        Text("OffSet")
+                        Slider(value: $petalOffset, in: -40...40)
+                            .padding([.horizontal, .bottom])
+                        
+                        Text("Width")
+                        Slider(value: $petalWidth, in: 0...100)
+                            .padding(.horizontal)
+                    }
+            }
+            .navigationBarTitle("Flower Animation", displayMode: .inline)
+            
+            
         }
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
